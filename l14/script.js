9ec8d64b287483a80c48ca4640b41d0cb5d9ec83
jaenudin0901 <jaenudinj913198@gmail.com>
2019-10-13 00:30:13 +0700

    merubah file index.html style.css script.js

--- l14/script.js
+++ l14/script.js
@@ -6,7 +6,7 @@
 // });
 
 // DOM  Travelsal
-const close = document.querySelectorAll('.close');
+// const close = document.querySelectorAll('.close');
 
 // for(let i = 0; i < close.length; i++) {
 	// close[i].addEventListener('click', function(e) {
@@ -17,15 +17,28 @@ const close = document.querySelectorAll('.close');
 
 // Cara lain
 
-close.forEach(function(el) {
-	el.addEventListener('click', function(e) {
-		e.target.parentElement.style.display =' none';
-	});
+// close.forEach(function(el) {
+// 	el.addEventListener('click', function(e) {
+// 		e.target.parentElement.style.display =' none';
+// 		e.preventDefault();
+// 		e.stopPropagation();
+// 	});
+// });
+
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function(card){
+// 	card.addEventListener('click', function(e){
+// 		alert('ok');
+// 	});
+// });
+
+const container = document.querySelector('.container');
+container.addEventListener('click', function(e){
+	if(e.target.className == 'close'){
+	   e.target.parentElement.style.display='none';
+	   e.preventDefault();
+	}
 });
 
-
-
-
-
-const nama = document.querySelector('.nama');
-console.log(nama .previousElementSibling );
+// const nama = document.querySelector('.nama');
+// console.log(nama .previousElementSibling );
